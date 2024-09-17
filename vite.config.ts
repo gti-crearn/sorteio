bcbe78b08e99252f4606db36fc70254162e4ee76
Jarbson Costa <jarbsonfc@gmail.com>
2024-09-17 15:03:50 -0300

    spa

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/sorteio/', // Coloque o nome do seu repositório aqui
 })
